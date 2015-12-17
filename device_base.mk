6dd869c9201ac3ca07c86dfe85a8a38ac8e20b94
xc-racer99 <xc-racer2@live.ca>
2015-12-16 20:19:08 -0800

    aries-common: Add in samsung compatability symbols
    Needed to fix RIL

--- device_base.mk
+++ device_base.mk
@@ -147,6 +147,10 @@ PRODUCT_PACKAGES += \
     mkfs.f2fs \
     fibmap.f2fs
 
+# RIL
+PRODUCT_PACKAGES += \
+    libsamsung_symbols
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
