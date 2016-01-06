e14d1769fcae703ef8fa3e9708b88611cd53c5df
xc-racer99 <xc-racer2@live.ca>
2016-01-05 16:58:10 -0800

    aries-common: Use gcc4.8
    Requires the memset fix (and the fix for the memset fix) in the kernel.
    Before, using gcc4.8+ preventing bootup on devices using the Samsung Audi oneNAND driver (ie T959V and T959P)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,7 +30,6 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a8
 TARGET_CPU_VARIANT := cortex-a8
-KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(strip $(HOST_OS))-x86/arm/arm-eabi-4.7/bin/"
 
 # Bionic stuff
 BOARD_USES_LEGACY_MMAP := true
