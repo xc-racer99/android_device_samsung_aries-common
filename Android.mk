52e492c6741c914b4118f3c6dc4b56b3ef15e682
xc-racer99 <xc-racer2@live.ca>
2016-02-27 13:36:03 -0800

    aries-common: Don't allow build on non-galaxys4g devices
    Needs some major updates first

--- Android.mk
+++ Android.mk
@@ -1,3 +1,12 @@
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),aries)
     include $(all-subdir-makefiles)
+# Don't allow building for non-galaxys4g devices for now
+ifeq ($(filter galaxys4gmtd telusgalaxys4gmtd,$(TARGET_DEVICE)),)
+    $(warning ****************************************************************************)
+    $(warning * This repo hasn't been updated for 6.0 save changes for galaxys4g devices.)
+    $(warning * Please update this repo to work before trying to build for other devices.)
+    $(warning * Of course, you'll also need to remove this message.)
+    $(warning ****************************************************************************)
+    $(error stopping)
+endif
 endif
