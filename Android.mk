577338519ce690e4450490810d98760f9fa1b385
xc-racer99 <xc-racer2@live.ca>
2016-02-28 15:10:05 -0800

    aries-common: Add fascinatemtd to build barrier

--- Android.mk
+++ Android.mk
@@ -1,9 +1,9 @@
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),aries)
     include $(all-subdir-makefiles)
 # Don't allow building for non-galaxys4g devices for now
-ifeq ($(filter galaxys4gmtd telusgalaxys4gmtd,$(TARGET_DEVICE)),)
+ifeq ($(filter fascinatemtd galaxys4gmtd telusgalaxys4gmtd,$(TARGET_DEVICE)),)
     $(warning ****************************************************************************)
-    $(warning * This repo hasn't been updated for 6.0 save changes for galaxys4g devices.)
+    $(warning * This repo hasn't been updated for 6.0 save changes for fascinate orgalaxys4g devices.)
     $(warning * Please update this repo to work before trying to build for other devices.)
     $(warning * Of course, you'll also need to remove this message.)
     $(warning ****************************************************************************)
