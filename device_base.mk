195b70988244020c847d57c69ff3c830e9ee10c5
xc-racer99 <xc-racer2@live.ca>
2016-06-27 22:21:33 -0700

    Revert "aries-common: Move some files from KERNEL_BINARIES to device_base.mk"
    Breaks make bootimage.  Not required for a common galaxys4g anyways
    This reverts commit ce6b5eb000d17de5eca93b2000707ed0460cdea3.

--- device_base.mk
+++ device_base.mk
@@ -69,13 +69,8 @@ PRODUCT_COPY_FILES += \
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-    bml_over_mtd \
     setup_fs \
-    static_busybox \
-    utility_erase_image \
-    utility_flash_image \
-    utility_make_ext4fs
-
+    bml_over_mtd
 
 # These are the OpenMAX IL configuration files
 PRODUCT_COPY_FILES += \
