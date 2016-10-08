f13196f186d02903abd4d3a18ef9f22ef0b29cc2
xc-racer99 <xc-racer2@live.ca>
2016-10-08 09:15:11 -0700

    aries-common: Remove dexoat max heap override
    Seems to slow the system down.

--- device_base.mk
+++ device_base.mk
@@ -186,7 +186,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-flags=--no-watch-dog \
     dalvik.vm.dex2oat-swap=false \
-    dalvik.vm.dex2oat-Xmx=256m \
     dalvik.vm.usejit=false \
     dalvik.vm.usejitprofiles=false
 
