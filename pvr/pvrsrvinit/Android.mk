3a56c8d0f1f53253d612e0d4bc15f1583047c5b0
D. Andrei Măceș <dmaces@nd.edu>
2017-10-24 13:01:00 -0700

    pvrsrvinit/libEGL_POWERVR: Allow -lsrv_{init,um}/-lIMGegl as LDLIBS
    Use the LOCAL_ALLOWED_LDLIBS whitelisting introduced in build/make, required for proper linking against the legacy DDK vendor blobs.

--- pvr/pvrsrvinit/Android.mk
+++ pvr/pvrsrvinit/Android.mk
@@ -3,11 +3,13 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := pvrsrvinit.c
-LOCAL_LDFLAGS := -L vendor/samsung/aries-common/proprietary
-LOCAL_LDLIBS := -lsrv_init -lsrv_um
-LOCAL_MODULE_PATH := $(TARGET_OUT)/bin/
+LOCAL_CFLAGS := -Wall -Werror
+LOCAL_LDFLAGS := -L $(TARGET_OUT_VENDOR)/lib
+LOCAL_ADDITIONAL_DEPENDENCIES := libsrv_init libsrv_um
+LOCAL_ALLOWED_LDLIBS := $(LOCAL_ADDITIONAL_DEPENDENCIES:lib%=-l%)
+LOCAL_LDFLAGS += $(LOCAL_ALLOWED_LDLIBS)
+LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_MODULE := pvrsrvinit
 LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES:= libc libdl libcutils
 
 include $(BUILD_EXECUTABLE)
