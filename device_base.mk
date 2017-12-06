b14edaad5e162de67d75fa2da6c8d693ce478348
xc-racer99 <xc-racer2@live.ca>
2017-12-05 19:03:55 -0800

    Inherit common GPS SUPL config

--- device_base.mk
+++ device_base.mk
@@ -254,5 +254,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.strictmode.visual=0
 
 include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk
-
+$(call inherit-product-if-exists, device/common/gps/gps_us_supl.mk)
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
